3db587c00cb4077d42e404010ce9c67bbee34a48
tahirburak2 <63349484+tahirburak2@users.noreply.github.com>
2024-09-10 05:32:04 +0300

    as

--- Arrays homework/soru1/obj/Debug/net8.0/soru1.AssemblyInfo.cs
+++ Arrays homework/soru1/obj/Debug/net8.0/soru1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("soru1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89e3c2dabc3a8da0345cb287a2ea44543c7446de")]
 [assembly: System.Reflection.AssemblyProductAttribute("soru1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("soru1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
